c7af126a567b7b3e888414bef8395d773255d301
liviuflore <liviuflore@gmail.com>
2016-12-21 16:54:30 +0200

    Update hass_installer.sh

--- hass_installer.sh
+++ hass_installer.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
-# Home Assistant Raspberry Pi Installer Kickstarter
-# Copyright (C) 2016 Jonathan Baginski - All Rights Reserved
-# Permission to copy and modify is granted under the MIT License
-# Last revised 5/15/2016
+# eg: ./hass_installer.sh -vsc /vagrant/hassconfig -h 192.168.1.21 -u pi
 
-fab_deploy_type=0
+virtualenv=False
+usessl=False
+config_path=''
+target_host=localhost
+target_user=$USER
 
-while getopts ":nv" opt; do
-    case $opt in
-        n)
-            fab_deploy_type=0
-            ;;
-        v)
-            fab_deploy_type=1
-            ;;
-        \?)
-            echo "Invalid option: -$OPTARG" >&2
-            ;;
+TEMP=`getopt -o vschu: --long virtual,ssl,config:,host:,user: -n 'hass_installer.sh' -- "$@"`
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$TEMP"
+
+while true ; do
+    case "$1" in
+        -v|--virtual) virtualenv=True ; shift ;;
+        -s|--ssl) usessl=True ; shift ;;
+        -c|--config) config_path=$2 ; shift 2 ;;
+        -h|--host) target_host=$2 ; shift 2 ;;
+        -u|--user) target_user=$2 ; shift 2 ;;
+        --) shift ; break ;;
+        \?) echo "Invalid option: -$OPTARG" >&2 ;;
     esac
 done
 
@@ -38,13 +41,13 @@ sudo /usr/bin/pip install fabric
 
 ## Run fab install ##
 
-echo "-- Run HASS installer [deploy type $fab_deploy_type]"
+echo "-- Run HASS installer [v:$virtualenv s:$usessl c:$config_path]"
 if [ ! -d $(dirname "$0")/.git ]; then
     echo "-- Git clone HASS installer"
     git clone https://github.com/liviuflore/home-assistant-installer.git
     cd home-assistant-installer
 fi
 
-fab deploy -H localhost 2>&1 | tee installation_report.txt
+fab deploy:venv=$virtualenv,ssl=$usessl,configuration=$config_path -H $target_host -u $target_user 2>&1 | tee installation_report.txt
 
 exit
